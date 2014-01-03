5052afc81057e6b5b0152eb431ed7f918aa14afc
Marcin Urbanski <0.660162@gmail.com>
2014-01-03 21:18:27 +0100

    prymitywnie komunikacja

--- server/server_main.c
+++ server/server_main.c
@@ -17,15 +17,13 @@
 
 int main(int argc, char* argv[])
 {
+	printf("Listening on port 33337\n");	
+
 	int status, addrlen, sockd;
 	struct sockaddr_in my_name, cli_name;
-/*
-	FsOpenServerS server_struct;
-	FsOpenServerC client_struct;
 
-	server_struct.answer = OK;
-	server_struct.server_handler = 1;
-*/
+	int server_handler = 5;
+
 	FsOpenServerS open_server_s;
 	FsOpenServerC open_server_c;
 	FsCloseServerS close_server_s;
@@ -33,12 +31,13 @@ int main(int argc, char* argv[])
 	
 	FsCommand command;
 	FsAnswer answer;
-	
+	/*
 	if (argc < 2)
 	{
 		fprintf(stderr, "Usage: %s port_number\n", argv[0]);
 		exit(1);
 	}
+	*/
 	/* create a socket */
 	sockd = socket(AF_INET, SOCK_DGRAM, 0);
 	if (sockd == -1)
@@ -49,14 +48,34 @@ int main(int argc, char* argv[])
 	/* server address */
 	my_name.sin_family = AF_INET;
 	my_name.sin_addr.s_addr = INADDR_ANY;
-	my_name.sin_port = htons (atoi (argv[1]));
+	my_name.sin_port = htons (33337);//atoi (argv[1]));
 	status = bind(sockd, (struct sockaddr*) &my_name, sizeof (my_name));
 
 	addrlen = sizeof(struct sockaddr_in);
+	recvfrom (sockd, (FsCommand*) &command, sizeof (int), 0, (struct sockaddr*) &cli_name, &addrlen);
+	answer = OK;
+	sendto (sockd, (int*) &answer, sizeof (int), 0, (struct sockaddr*) &cli_name, sizeof (cli_name));
 
-	status = recvfrom (sockd, (FsOpenServerC*) &open_server_c, sizeof (FsOpenServerC), 0, (struct sockaddr*) &cli_name, &addrlen);	
-	status = sendto (sockd, (FsOpenServerS*) &open_server_s, sizeof (FsOpenServerS), 0, (struct sockaddr*) &cli_name, sizeof (cli_name));
+	if (command == OPEN_SERVER)
+	{
+		recvfrom (sockd, (FsOpenServerC*) &open_server_c, sizeof (FsOpenServerC), 0, (struct sockaddr*) &cli_name, &addrlen);
+		open_server_s.server_handler = server_handler;	
+		status = sendto (sockd, (FsOpenServerS*) &open_server_s, sizeof (FsOpenServerS), 0, (struct sockaddr*) &cli_name, sizeof (cli_name));
+	} 
 
+	addrlen = sizeof(struct sockaddr_in);
+	recvfrom (sockd, (FsCommand*) &command, sizeof (int), 0, (struct sockaddr*) &cli_name, &addrlen);		
+	answer = OK;	
+	sendto (sockd, (int*) &answer, sizeof (int), 0, (struct sockaddr*) &cli_name, sizeof (cli_name));
+	
+	if (command == CLOSE_SERVER)
+	{		
+		recvfrom (sockd, (FsCloseServerC*) &close_server_c, sizeof (FsCloseServerC), 0, (struct sockaddr*) &cli_name, &addrlen);
+		printf ("Closing session with: %d\n", close_server_c.server_handler); 
+		close_server_s.answer = OK;
+		status = sendto (sockd, (FsCloseServerS*) &close_server_s, sizeof (FsCloseServerS), 0, (struct sockaddr*) &cli_name, sizeof (cli_name));
+	} 	
+	
 	close(sockd);
 	return 0;
 }
--- tin_library/tin_library.c
+++ tin_library/tin_library.c
@@ -8,6 +8,8 @@
  * wysylamy login a serwer jesli odpowie dobrze to zwracamy uchwyt
  */ 
 
+#define PORT 33337
+
 int fs_open_server (char* server_address)
 {
 	int sockd, addrlen, port, status, count;
@@ -34,45 +36,93 @@ int fs_open_server (char* server_address)
   	// server address 
 	srv_addr.sin_family = AF_INET;    
 	inet_aton ("localhost", &srv_addr.sin_addr);
+	
 	printf ("input port: ");
 	scanf ("%d", &port);
-    srv_addr.sin_port = htons(port);
+    srv_addr.sin_port = htons(PORT);
 
 	addrlen = sizeof(struct sockaddr_in);
 
-	status = sendto (sockd, (FsOpenServerC*) &open_server_c, sizeof(FsOpenServerC), 0, (struct sockaddr*) &srv_addr, sizeof(srv_addr));
-	addrlen = sizeof(srv_addr);
-  	count = recvfrom(sockd, (FsOpenServerS*) &open_server_s, sizeof(FsOpenServerS), 0, (struct sockaddr*)&srv_addr, &addrlen);
+	int com = command;
 
+	sendto (sockd, (int*) &command, sizeof(int), 0, (struct sockaddr*) &srv_addr, sizeof(srv_addr));
+	addrlen = sizeof(struct sockaddr_in);
+	recvfrom(sockd, (FsAnswer*) &answer, sizeof(int), 0, (struct sockaddr*)&srv_addr, &addrlen);
+	
+	if (answer == OK)
+	{
+		sendto (sockd, (FsOpenServerC*) &open_server_c, sizeof(FsOpenServerC), 0, (struct sockaddr*) &srv_addr, sizeof(srv_addr));
+	  	recvfrom(sockd, (FsOpenServerS*) &open_server_s, sizeof(FsOpenServerS), 0, (struct sockaddr*)&srv_addr, &addrlen);
+	}
     close (sockd);
     
 	return 0;//server_struct.server_handler;
 }
 
-int fs_close_server (int srvhndl)
+int fs_close_server (int server_handler)
+{
+	int sockd, addrlen, status, count;
+    struct sockaddr_in my_addr, srv_addr;
+
+	FsCommand command = CLOSE_SERVER;
+	FsAnswer answer;
+	FsCloseServerS close_server_s;
+	FsCloseServerC close_server_c = {server_handler};
+	
+	sockd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sockd == -1)
+    {
+        perror("Socket creation error");
+        exit(1);
+    }
+	
+	// client address
+	my_addr.sin_family = AF_INET;
+    my_addr.sin_addr.s_addr = INADDR_ANY;
+    my_addr.sin_port = 0;
+    status = bind (sockd, (struct sockaddr*)&my_addr, sizeof(my_addr));
+
+  	// server address 
+	srv_addr.sin_family = AF_INET;    
+	inet_aton ("localhost", &srv_addr.sin_addr);
+    srv_addr.sin_port = htons(PORT);
+	
+	status = sendto (sockd, (int*) &command, sizeof(int), 0, (struct sockaddr*) &srv_addr, sizeof(srv_addr));
+	addrlen = sizeof(struct sockaddr_in);
+	recvfrom(sockd, (int*) &answer, sizeof(int), 0, (struct sockaddr*)&srv_addr, &addrlen);
+	if (answer == OK)
+	{
+		status = sendto (sockd, (FsCloseServerC*) &close_server_c, sizeof(FsCloseServerC), 0, (struct sockaddr*) &srv_addr, sizeof(srv_addr));
+	  	recvfrom(sockd, (FsCloseServerS*) &close_server_s, sizeof(FsCloseServerS), 0, (struct sockaddr*)&srv_addr, &addrlen);
+	}
+
+	printf ("Closing server: %d\n", close_server_s.answer); 
+	
+    close (sockd);
+    
+	return 0;
+}
+
+int fs_open (int server_handler, char* name, int flags)
 {
 }
 
-int fs_open (int srvhndl, char* name, int flags)
+int fs_write (int server_handler, int fd, void *buf, size_t len)
 {
 }
 
-int fs_write (int srvhndl, int fd, void *buf, size_t len)
+int fs_read (int server_handler, int fd, long offset, int whence)
 {
 }
 
-int fs_read (int srvhndl, int fd, long offset, int whence)
+int fs_close (int server_handler, int fd)
 {
 }
 
-int fs_close (int srvhndl, int fd)
-{
-}
-
-/*int fs_fstat (int srvhndl, int fd, struct stat *buf)
+/*int fs_fstat (int server_handler, int fd, struct stat *buf)
 {
 }
 */
-int fs_lock (int srvhndl, int fd, int mode)
+int fs_lock (int server_handler, int fd, int mode)
 {
 }
