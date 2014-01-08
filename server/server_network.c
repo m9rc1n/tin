6795aa806817f96c2447a5f3c076b11bdb527924
Marcin Urbanski <0.660162@gmail.com>
2014-01-09 00:13:45 +0100

    poprawa bledow po zmianie protokolu

--- server/server_network.c
+++ server/server_network.c
@@ -4,14 +4,14 @@
 int s_open_server(IncomingRequest *inc_request)
 {
     int session_id;
-    
+
     session_id = session_create();
-    
+
 	printf ("Opening session with: %d\n", session_id);
     FsResponse response;
-    response.response_data.open_server_s.server_handler = session_id;
+    response.response_data.open_server.server_handler = session_id;
     response.answer = ACCEPTED;
-    
+
     printf("%s\n", inet_ntoa(inc_request->client_addr.sin_addr));
 
 	return sendto(sockd, &response, sizeof (FsResponse), 0, (struct sockaddr*) &(inc_request->client_addr), inc_request->client_addr_len);
@@ -20,14 +20,14 @@ int s_open_server(IncomingRequest *inc_request)
 int s_close_server(IncomingRequest *inc_request)
 {
     int session_id;
-    
-    session_id = inc_request->request.request_data.close_server_c.server_handler;
-    
+
+    session_id = inc_request->request.request_data.close_server.server_handler;
+
     session_close(session_id);
-    
+
 	printf("Closing session with: %d\n", session_id);
     FsResponse response;
     response.answer = CLOSED;
-   
+
 	return sendto(sockd, &response, sizeof (FsResponse), 0,(struct sockaddr*) &(inc_request->client_addr), inc_request->client_addr_len);
 }
