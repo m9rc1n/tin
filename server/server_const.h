848fec262b5d5fdbca049fb57ded06ad4e08fa17
Amadeusz Kosik <eternalsoulharvester@gmail.com>
2014-01-09 20:34:20 +0100

    Dodany półautomatyczny test wywalania timeoutów i poprawiony Makefile dla testów.

--- server/server_const.h
+++ server/server_const.h
@@ -33,7 +33,7 @@
  * Okres od ostatniego requesta po upłynięciu którego sesja jest uznana jako zombie w sekundach.
  */
 #ifndef SESSION_ZOMBIE_TIME
-#define SESSION_ZOMBIE_TIME 300
+#define SESSION_ZOMBIE_TIME 15
 #endif
 
 
