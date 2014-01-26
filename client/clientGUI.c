703f7363f0bf2e40f65cc90dc886d2346343bd47
Rafal Stupienko <rafal.stupienko@coi.gov.pl>
2014-01-26 13:12:38 +0100

    Blokady male

--- client/clientGUI.c
+++ client/clientGUI.c
@@ -3,12 +3,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/stat.h>
+#include <sys/wait.h>
+#include <sys/types.h>
 #include <fcntl.h>
 #include "ncurses-menu.h"
 #include "ncurses-readstring.h"
 #include "../tin_library/tin_library.h"
 
-char address[20], portString[20], fileName[50], readBufor[512], writeBufor[512], size[15], mode[15], atime[15], cctime[15], mtime[15];
+char address[20], portString[20], fileName[50], readBufor[512], writeBufor[512], size[15], mode[15], atime[15], cctime[15], mtime[15], offset[15];
 volatile int serverHandler;
 volatile int fileDescriptor; 
 struct stat* buf;
@@ -56,7 +58,7 @@ int main(int argc, char *argv[])
 	char mainMenu[][100] = {{"Connect"}, {"Setup"}, {"Quit"},};
 	char configMenu[][100] = {{"Address"}, {"Port"}, {"Save config"}, {"Load config"}, {"Return"},};
 	char connectedMenu[][100] = {{"Run test"},{"Create file"},{"Open file"},{"Disconnect"}}; 
-	char fileMenu[][100] = {{"Read file"},{"Write file"},{"Edit file"},{"File info"},{"Return"},};
+	char fileMenu[][100] = {{"Read file"},{"Read file from"},{"Write file"},{"Edit file"}, {"Edit file from"},{"File info"},{"Return"},};
 	char testMenu[][100] = {{"Test Open"},{"Test Read"},{"Test Write"},{"Simple test"},{"Basic session"},{"Session timeout"},{"Return"},};
 
 	setlocale(LC_CTYPE, "");
@@ -82,6 +84,8 @@ int main(int argc, char *argv[])
 			mvprintw(COORD_Y + 3,COORD_X + WIDTH + 3, "Connecting...");
 			//laczymy sie z serwerem
 			serverHandler = fs_open_server(address, strtol(portString, NULL, 0));
+			if(serverHandler != -1)
+{
 			erase();
 			do
 			{
@@ -92,6 +96,7 @@ int main(int argc, char *argv[])
 					do
 					{
 						testMenuReturn = print_menu(COORD_Y,COORD_X,7,WIDTH,"Tests Menu",testMenu,testMenuReturn);
+						int errorCode = 0;
 						if(testMenuReturn == 1)
 						{
 							erase();
@@ -100,8 +105,11 @@ int main(int argc, char *argv[])
 							if(pid == 0)
 							{
 								execv("../tests/bin/file_opener.out", arguments);
+								wait(&errorCode);								
 							}
 							sleep(2);
+							if(errorCode == 0){mvprintw(20,1, "Test success");}else{mvprintw(20,1, "Test failed");}
+							getch();
 						}
 						else if (testMenuReturn == 2)
 						{
@@ -110,8 +118,11 @@ int main(int argc, char *argv[])
 							if(pid == 0)
 							{
 								execv("../tests/bin/file_reader.out", arguments);
+								wait(&errorCode);
 							}
 							sleep(2);
+							if(errorCode == 0){mvprintw(20,1, "Test success");}else{mvprintw(20,1, "Test failed");}
+							getch();
 						}
 						else if (testMenuReturn == 3)
 						{
@@ -120,8 +131,11 @@ int main(int argc, char *argv[])
 							if(pid == 0)
 							{
 								execv("../tests/bin/file_writer.out", arguments);
+								wait(&errorCode);
 							}
 							sleep(2);
+							if(errorCode == 0){mvprintw(20,1, "Test success");}else{mvprintw(20,1, "Test failed");}
+							getch();
 						}
 						else if (testMenuReturn == 4)
 						{
@@ -130,8 +144,11 @@ int main(int argc, char *argv[])
 							if(pid == 0)
 							{
 								execv("../tests/bin/file_simple.out", arguments);
+								wait(&errorCode);
 							}
 							sleep(2);
+							if(errorCode == 0){mvprintw(20,1, "Test success");}else{mvprintw(20,1, "Test failed");}
+							getch();
 						}
 						else if (testMenuReturn == 5)
 						{
@@ -140,8 +157,11 @@ int main(int argc, char *argv[])
 							if(pid == 0)
 							{
 								execv("../tests/bin/session_basic.out", arguments);
+								wait(&errorCode);
 							}
 							sleep(2);
+							if(errorCode == 0){mvprintw(20,1, "Test success");}else{mvprintw(20,1, "Test failed");}
+							getch();
 						}
 						else if (testMenuReturn == 6)
 						{
@@ -150,8 +170,11 @@ int main(int argc, char *argv[])
 							if(pid == 0)
 							{
 								execv("../tests/bin/session_timeouter.out", arguments);
+								wait(&errorCode);
 							}
 							sleep(2);
+							if(errorCode == 0){mvprintw(20,1, "Test success");}else{mvprintw(20,1, "Test failed");}
+							getch();
 						}
 						erase();
 					} while(testMenuReturn != 7);
@@ -169,7 +192,7 @@ int main(int argc, char *argv[])
 					erase();
 					do
 					{
-						fileMenuReturn = print_menu(COORD_Y,COORD_X,5,WIDTH,"File operations",fileMenu,fileMenuReturn);
+						fileMenuReturn = print_menu(COORD_Y,COORD_X,7,WIDTH,"File operations",fileMenu,fileMenuReturn);
 						if(fileMenuReturn == 1)
 						{
 							mvreadstr(COORD_Y + 3,COORD_X + WIDTH + 5, fileName, 18, 0);
@@ -182,10 +205,10 @@ int main(int argc, char *argv[])
 							fs_close(serverHandler, fileDescriptor);
 							erase();
 						}
-						else if(fileMenuReturn == 2)
+						else if(fileMenuReturn == 3)
 						{
 							//otwarcie pliku
-							mvreadstr (COORD_Y + 5,COORD_X + WIDTH + 5, fileName, 18, 0);
+							mvreadstr (COORD_Y + 7,COORD_X + WIDTH + 5, fileName, 18, 0);
 							fileDescriptor = fs_open(serverHandler, fileName, O_WRONLY);
 							//zapis
 							erase();
@@ -202,9 +225,9 @@ int main(int argc, char *argv[])
 							fs_write(serverHandler, fileDescriptor, writeBufor, i);
 							fs_close(serverHandler, fileDescriptor);
 						}
-						else if(fileMenuReturn == 3)
+						else if(fileMenuReturn == 4)
 						{
-							mvreadstr(COORD_Y + 7,COORD_X + WIDTH + 5, fileName, 18, 0);
+							mvreadstr(COORD_Y + 9,COORD_X + WIDTH + 5, fileName, 18, 0);
 							fileDescriptor = fs_open(serverHandler, fileName, O_WRONLY);
 							fs_lseek(serverHandler, fileDescriptor, 0, SEEK_END);
 							erase();
@@ -220,10 +243,9 @@ int main(int argc, char *argv[])
 							fs_write(serverHandler, fileDescriptor, writeBufor, i);
 							fs_close(serverHandler, fileDescriptor);
 						}
-						else if(fileMenuReturn == 4)
+						else if(fileMenuReturn == 6)
 						{
-							//TODO - leci seg. fault nie wiedziec czemu :/
-							mvreadstr (COORD_Y + 9,COORD_X + WIDTH + 5, fileName, 18, 0);
+							mvreadstr (COORD_Y + 13,COORD_X + WIDTH + 5, fileName, 18, 0);
 							fileDescriptor = fs_open(serverHandler, fileName, O_RDONLY);
 							erase();
 							buf = (struct stat*) calloc(sizeof(struct stat), 1);
@@ -256,7 +278,46 @@ int main(int argc, char *argv[])
 							free(buf);
 							fs_close(serverHandler, fileDescriptor);
 						}
-					} while(fileMenuReturn != 5);
+						else if(fileMenuReturn == 2)
+						{
+							//nazwa pliku
+							mvreadstr(COORD_Y + 5,COORD_X + WIDTH + 5, fileName, 18, 0);
+							//offset
+							mvreadstr(COORD_Y + 5,COORD_X + WIDTH + 20, offset, 18, 0);
+							
+
+							fileDescriptor = fs_open(serverHandler, fileName, O_RDONLY);
+							fs_lseek(serverHandler, fileDescriptor, atoi(offset), SEEK_SET);
+							//odczyt
+							erase();
+							fs_read(serverHandler, fileDescriptor, &readBufor, sizeof(readBufor));
+							mvprintw(1,1,readBufor);
+							getch();
+							fs_close(serverHandler, fileDescriptor);
+							erase();
+							
+						}
+						else if(fileMenuReturn == 5)
+						{
+							mvreadstr(COORD_Y + 11,COORD_X + WIDTH + 5, fileName, 18, 0);
+							mvreadstr(COORD_Y + 11,COORD_X + WIDTH + 20, offset, 18, 0);
+
+							fileDescriptor = fs_open(serverHandler, fileName, O_WRONLY);
+							fs_lseek(serverHandler, fileDescriptor, atoi(offset), SEEK_SET);
+							erase();
+							mvreadstr(COORD_Y - 3,COORD_X - 9, writeBufor, 512, 0);
+							char  ch = writeBufor[0];
+							int i = 0;
+							while(ch != ';')
+							{
+								i += 1;
+								ch = writeBufor[i];
+							}
+							erase();
+							fs_write(serverHandler, fileDescriptor, writeBufor, i);
+							fs_close(serverHandler, fileDescriptor);
+						}
+					} while(fileMenuReturn != 7);
 					erase();
 					
 				}
@@ -267,6 +328,7 @@ int main(int argc, char *argv[])
 				}
 
 			} while(connectedMenuReturn != 4);
+}
 
 		}
 		else if(mainMenuReturn == 2) //SETTING MENU
